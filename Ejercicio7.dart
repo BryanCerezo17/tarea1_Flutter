384be6afb9989432b93db8dc362e3f3f0f9cbea1
BryanCerezo17 <101740242+BryanCerezo17@users.noreply.github.com>
2022-03-17 10:51:05 -0500

    Create Ejercicio7.dart

---
+++ Ejercicio7.dart
@@ -0,0 +1,9 @@
+/*7.- Crear una función que me retorne una Lista de String*/
+
+void main() {
+  print(funcionRetornar());
+}
+
+  List <String> funcionRetornar(){
+    return ['Hola','Mundo','Bryan','Juan','17'];
+  }
