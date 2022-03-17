47902a9a49f8cd6db512c28daa960225d0e90fa7
BryanCerezo17 <101740242+BryanCerezo17@users.noreply.github.com>
2022-03-16 23:21:30 -0500

    Update Ejercicio2.dart

--- Ejercicio2.dart
+++ Ejercicio2.dart
@@ -9,6 +9,10 @@ void main() {
   print(multiplicacion(numero1,numero2));
   print(division(numero1,numero2));
   
+  suma1(numero1,numero2);
+  resta1(numero1,numero2);
+  multiplicacion1(numero1,numero2);
+  division1(numero1,numero2);
 }
   double suma(double n1, double n2){
     return n1+n2;
@@ -24,4 +28,22 @@ void main() {
   
   double division(double n1, double n2){
     return n1/n2;
+  }
+
+
+
+void suma1(double n1, double n2){
+    print(n1+n2);
+  }
+  
+  void resta1(double n1, double n2){
+    print(n1-n2);
+  }
+  
+  void multiplicacion1(double n1, double n2){
+    print(n1*n2);
+  }
+  
+  void division1(double n1, double n2){
+    print(n1/n2);
   }
