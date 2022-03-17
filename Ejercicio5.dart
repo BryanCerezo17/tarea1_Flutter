04ddc9f6edd9b21c091d3c49aac073a087c41c22
BryanCerezo17 <101740242+BryanCerezo17@users.noreply.github.com>
2022-03-16 23:49:17 -0500

    Create Ejercicio5.dart

---
+++ Ejercicio5.dart
@@ -0,0 +1,11 @@
+/*5.- Crear una función y como parámetro me permita 
+ * recibir una lista de String (pintar los valores)*/
+
+void main() {
+  List<String> animales = ['Conejo', 'Gato','Tortuga','Perro','Aguila'];
+  funcionLista(animales);
+}
+
+  void funcionLista(List <String> lista){
+    print(lista);
+  }
